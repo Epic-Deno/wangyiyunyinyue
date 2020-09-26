8f0ff2a1505310136fa4eefb55f7b658b085385f
zhangzhen <1557549413@qq.com>
2020-09-27 00:16:56 +0800

    批注

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
+/*
+ * @Description: It was written by Pony
+ * @Version: 2.0
+ * @Autor: Pony
+ * @Date: 2020-09-27 00:11:20
+ * @LastEditors: Pony
+ * @LastEditTime: 2020-09-27 00:16:26
+ */
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <></>
   );
 }
 
