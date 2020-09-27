c3f06d21f57f9ce468b301cff6e67463e31e9afe
zhangzhen <1557549413@qq.com>
2020-09-28 00:10:20 +0800

    完善

--- src/App.js
+++ src/App.js
@@ -4,15 +4,157 @@
  * @Autor: Pony
  * @Date: 2020-09-27 00:11:20
  * @LastEditors: Pony
- * @LastEditTime: 2020-09-27 00:16:26
+ * @LastEditTime: 2020-09-28 00:06:48
  */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <></>
-  );
+import Axios from "axios";
+import Bricks from "bricks.js"; //瀑布流布局插件
+//评论请求
+function getData(limit = 50, offset) {
+  return Axios.get("https://musicapi.leanapp.cn/comment/music", {
+    params: {
+      id: 518686034,
+      limit,
+      offset, // 倒叙
+    },
+  }).then((res) => res.data);
 }
 
+function dateFormat(timestamp, formats) {
+  formats = formats || "Y-m-d";
+
+  const zero = (value) => {
+    if (value < 10) {
+      return "0" + value;
+    }
+    return value;
+  };
+
+  const myDate = timestamp ? new Date(timestamp) : new Date();
+
+  const year = myDate.getFullYear();
+  const month = zero(myDate.getMonth() + 1);
+  const day = zero(myDate.getDate());
+
+  const hour = zero(myDate.getHours());
+  const minite = zero(myDate.getMinutes());
+  const second = zero(myDate.getSeconds());
+
+  return formats.replace(/Y|m|d|H|i|s/gi, function (matches) {
+    return {
+      Y: year,
+      m: month,
+      d: day,
+      H: hour,
+      i: minite,
+      s: second,
+    }[matches];
+  });
+}
+
+let page = 1;
+let hotCommentInst;
+let commentInst;
+
+function App() {
+  const [hotComments, setHotComments] = useState([]);
+  const [comments, setComments] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const loadMore = () => {
+    if (loading) return;
+    setLoading(true);
+    getData(50, 2027450 - page * 50)
+      .then((data) => {
+        setComments(comments.concat(data.comments));
+        page === 1 ? commentInst.pack() : commentInst.update();
+        page++;
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    getData(20).then((data) => {
+      setHotComments(data.hotComments);
+      hotCommentInst.pack();
+    });
+    loadMore();
+    const sizeOpt = [
+      { columns: 2, gutter: 10 },
+      { mq: "600px", columns: 3, gutter: 10 },
+      { mq: "800px", columns: 4, gutter: 10 },
+      { mq: "1000px", columns: 5, gutter: 16 },
+      { mq: "1130px", columns: 5, gutter: 16 },
+    ];
+    hotCommentInst = Bricks({
+      container: "#hot-comment-list",
+      packed: "data-packed",
+      sizes: sizeOpt,
+    });
+    commentInst = Bricks({
+      container: "#comment-list",
+      packed: "data-packed",
+      sizes: sizeOpt,
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1 className="title">-戒烟の网易云评论-</h1>
+      <div style={{ textAlign: 'center' }}>
+        <iframe className="linkFrame" src="//player.bilibili.com/player.html?aid=29353549&amp;cid=44921106&amp;bvid=BV1Ws411V76b&amp;danmaku=0&amp;page=1" style={{ width: '800px', height: '500px' }} title="jieyan" frameBorder="no" framespacing="0" allowFullScreen={true} scrolling="no" border="0"></iframe>
+      </div>
+      <h2 className="subtitle">
+        热评 TOP20
+      </h2>
+      <div id="hot-comment-list">
+        {hotComments.map(({ user, content, commentId, time }) => (
+          <CommentCard
+            key={commentId+time}
+            avatarUrl={user.avatarUrl}
+            nickname={user.nickname}
+            content={content}
+            time={time}
+          />
+        ))}
+      </div>
+      <h2 className="subtitle">评论回忆</h2>
+      <div id="comment-list">
+        {comments.map(({ user, content, commentId, time }) => (
+          <CommentCard
+            key={commentId+time}
+            avatarUrl={user.avatarUrl}
+            nickname={user.nickname}
+            content={content}
+            time={time}
+          />
+        ))}
+      </div>
+      <div className="load-more">
+        <a className="load-more-button" onClick={loadMore}>
+          {loading ? "..." : "加载更多"}
+        </a>
+      </div>
+    </div>
+  );
+};
+
+//评论卡片
+function CommentCard({ avatarUrl, nickname, content, time }) {
+  return (
+    <div className="grid-item" style={{ backgroundColor: "#fff" }}>
+      <div className="userinfo">
+        <img className="avatar" src={avatarUrl} alt="avatar" />
+      </div>
+      <div className="content">
+        <span>{content}</span>
+        <div className="nickname">- {nickname}</div>
+        <div className="time">{dateFormat(time)}</div>
+      </div>
+    </div>
+  );
+};
 export default App;
---
+++ src/utils/api.js
@@ -0,0 +1,66 @@
+/*
+ * @Description: It was written by Pony
+ * @Version: 2.0
+ * @Autor: Pony
+ * @Date: 2020-09-27 23:45:05
+ * @LastEditors: Pony
+ * @LastEditTime: 2020-09-27 23:46:58
+ */
+/**
+ * 这个文件的代码是为了获取所有评论内容，然后保存在一个json文件里。
+ * 这样网页端就可以实现搜索评论的功能。
+ * 但发现只能查前1000个评论内容和后1000个，中间的都返回空数组，暂时还没有解决。
+ */
+const Axios = require("axios");
+const fs = require("fs");
+
+const TOTAL = 2027400;
+
+function getData(limit = 50, offset, before) {
+  return Axios.get("https://musicapi.leanapp.cn/comment/music", {
+    params: {
+      id: 518686034,
+      limit,
+      offset,
+      before,
+    },
+  }).then((res) => res.data);
+}
+
+let times = TOTAL / 50;
+const commentMap = {};
+let page = 1;
+async function recursive(before) {
+  if (page === times) {
+    fs.writeFileSync("./commentData.json", JSON.stringify(commentMap, null, 2));
+    return;
+  }
+  let _before;
+  try {
+    const { comments } = await getData(50, 50 * (page - 1), before);
+    console.log(`第${page}次请求  共有${comments.length}条评论`);
+    comments.forEach(({ user, time, content }) => {
+      if (commentMap[user.userId]) {
+        commentMap[user.userId].push({
+          nickname: user.nickname,
+          time,
+          content,
+        });
+      } else {
+        commentMap[user.userId] = [
+          {
+            nickname: user.nickname,
+            time,
+            content,
+          },
+        ];
+      }
+    });
+    _before = comments[comments.length - 1].time;
+  } catch (error) {
+    console.log(error);
+  }
+  page++;
+  await recursive(_before);
+}
+recursive();
