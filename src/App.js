065c414ebd9096eeeca2a954912d25ec5a933879
zhangzhen <1557549413@qq.com>
2020-10-06 00:35:32 +0800

    评论换成Dream it possible

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@
  * @Autor: Pony
  * @Date: 2020-09-27 00:11:20
  * @LastEditors: Pony
- * @LastEditTime: 2020-09-28 00:25:27
+ * @LastEditTime: 2020-10-06 00:30:34
  */
 import React, { useEffect, useState } from 'react';
 import './App.css';
@@ -14,7 +14,7 @@ import Bricks from "bricks.js"; //瀑布流布局插件
 function getData(limit = 50, offset) {
   return Axios.get("https://musicapi.leanapp.cn/comment/music", {
     params: {
-      id: 1293886117,
+      id: 38592976,
       limit,
       offset, // 倒叙
     },
@@ -103,9 +103,9 @@ function App() {
 
   return (
     <div className="App">
-      <h1 className="title">-戒烟の网易云评论-</h1>
+      <h1 className="title">-《Dream It Possible》の网易云评论-</h1>
       <div style={{ textAlign: 'center' }}>
-        <iframe className="linkFrame" src="//player.bilibili.com/player.html?aid=29353549&amp;cid=51010798&amp;bvid=BV1gs411M7LM&amp;danmaku=0&amp;page=1" style={{ width: '800px', height: '500px' }} title="jieyan" frameBorder="no" framespacing="0" allowFullScreen={true} scrolling="no" border="0"></iframe>
+        <iframe className="linkFrame" src="//player.bilibili.com/player.html?aid=48803444&amp;cid=85548037&amp;bvid=BV16b411T7JV&amp;danmaku=0&amp;page=1" style={{ width: '800px', height: '500px' }} title="jieyan" frameBorder="no" framespacing="0" allowFullScreen={true} scrolling="no" border="0"></iframe>
       </div>
       <h2 className="subtitle">
         热评 TOP20
